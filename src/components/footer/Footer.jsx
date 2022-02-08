d37bb0805ac79cfdcb057e5402e8086e9d34cabc
georgemwangi118 <gmwangi118@yahoo.com>
2022-02-08 21:59:51 +0300

    styled the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,35 +6,33 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   return (
-    <div className="container" id="footer">
-      <div className="footer">
-        <div className="c">GM</div>
-        <div className="c">
-          <p>&copy; Copyright 2022. All right reserved</p>
-        </div>
-        <div className="icons">
-          <a
-            href="https://twitter.com/mr_robot118"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <TwitterIcon />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/george-mwangi-47057215b/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <LinkedInIcon />
-          </a>
-          <a
-            href="https://github.com/georgemwangi118"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <GitHubIcon />
-          </a>
-        </div>
+    <div className="container footer" id="footer">
+      <div className="footer-logo">GM</div>
+      <div className="copyright">
+        <p>&copy; Copyright 2022. All right reserved</p>
+      </div>
+      <div className="icons">
+        <a
+          href="https://twitter.com/mr_robot118"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <TwitterIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/george-mwangi-47057215b/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
+        <a
+          href="https://github.com/georgemwangi118"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GitHubIcon />
+        </a>
       </div>
     </div>
   );
